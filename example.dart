ce44cc8daa4d2d4761a403ec2f9b1e638bd04b97
Vladyslav Latsko <vladislav.lacko@gmail.com>
2023-01-23 17:52:48 +0100

    User Class prototype

--- example.dart
+++ example.dart
@@ -1,5 +1,27 @@
+// parent Class : User
+// Holds details about an user
+// Has a function to show user details
+//
+// Child Class : Bank
+// Stores details about the account balance
+// Stores details about the amount
+// Allows for deposits, withdraw, view balance
+
+
 void main() {
-  String vitalii = "Vitalii";
-  String vlv = "Vlad";
-  print("My name is $vlv and my friend's name is $vitalii");
+  User bob = User("Bobby Fisher", 32);
+  bob.display();
+  bob.name = "Bob";
+  bob.display();
+}
+
+class User {
+  String name;
+  int age;
+
+  User(this.name, this.age);
+
+  void display() {
+    print("User name: $name, User age: $age");
+  }
 }
