fc25b02733958694c0d43855954d0678607b3d3e
Vladyslav Latsko <vladislav.lacko@gmail.com>
2023-02-02 17:34:19 +0100

    continuous switch; options 2, 3, 5, 6 are ready

--- example.dart
+++ example.dart
@@ -1,19 +1,16 @@
 import 'dart:io';
 import 'package:args/args.dart';
+import 'package:test/scaffolding.dart';
 
 class User {
   String name;
   int age;
+  // int pass;
   User(this.name, this.age);
-
-  void display(){
-    print("User name: $name, age: $age");
-  }
 }
 
 class BankAccount extends User {
   BankAccount(super.name, super.age);
-
   double _balance = 0;
   double get balance => _balance;
 
@@ -25,23 +22,100 @@ class BankAccount extends User {
     if (amount <= _balance) {
       _balance -= amount;
       return true;
+    } else {
+      print("You don't have enough money to withdraw");
+      return false;
     }
-    return false;
   }
 
   showBalance(){
-    print("Your balance: $balance");
+    print("Your balance: $balance \n");
   }
 }
 
+
 void main() {
-  var account = BankAccount("Bob", 43);
+  var bob = BankAccount("Bob", 43);
 
-  account.deposit(114415.54);
-  print(account.balance);
+  Parser(bob);
+  // var a = userKey();
+  // print("User-Login is: $a");
+}
 
-  account.display();
-  account.showBalance();
-  account.withdraw(4415.54);
-  account.showBalance();
+void Menu() {
+  print(
+      "Choose the option below: \n"
+      "1. Create account \n"
+      "2. Deposit money on your bank account\n"
+      "3. Withdraw money from your bank account\n"
+      "4. Money transfer\n"
+      "5. Show balance\n"
+      "6. Exit \n"
+  );
+}
+
+String Function() userKey = (){
+  print("Enter your name: \n");
+  String input = stdin.readLineSync()!;
+  return input;
+};
+
+Parser(obj){
+  Menu();
+    // while loop for continuous running switch
+    while (true) {
+      int input = int.parse(stdin.readLineSync()!);
+      switch (input) {
+        case 1:
+          {
+            print("number $input");
+          }
+          break;
+        case 2:
+          {
+            print("Type the amount of money to deposit: \n");
+            double input = double.parse(stdin.readLineSync()!);
+            obj.deposit(input);
+            obj.showBalance();
+          }
+          continue menu;
+        case 3:
+          {
+            print("Type the amount of money to withdraw: \n");
+            double input = double.parse(stdin.readLineSync()!);
+            obj.withdraw(input);
+            obj.showBalance();
+          }
+          continue menu;
+        case 4:
+          {
+            print("number $input");
+          }
+          break;
+        case 5:
+          {
+            obj.showBalance();
+          }
+          continue menu;
+
+        case 6: {
+          return input;
+        }
+
+        menu: case 7:
+        {
+          Menu();
+        }
+        break;
+        default:
+        {
+          print("Something went wrong, try choosing the option again: \n");
+        }
+        continue menu;
+      }
+      // if user choose option 6 —> exit
+      if (input == 6){
+        break;
+      }
+    }
 }
